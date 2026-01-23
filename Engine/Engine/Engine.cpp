50b13dc455be39e651c4ce8b9aec1d7d1e58e67c
chanyoung <kmklko453@gmail.com>
2026-01-23 17:30:47 +0900

    오류 수정

--- Engine/Engine/Engine.cpp
+++ Engine/Engine/Engine.cpp
@@ -8,7 +8,7 @@ namespace Wanted
 	{
 	}
 
-	Engine::~Engine() 
+	Engine::~Engine()
 	{
 	}
 
@@ -22,11 +22,11 @@ namespace Wanted
 		//__int64
 		int64_t currentTime = 0;
 		int64_t previousTime = 0;
-	
+
 		//하드웨어 타이머로 시간 구하기
-		LARGE_INTEGER time; 
+		LARGE_INTEGER time;
 		QueryPerformanceCounter(&time);
-		
+
 		//엔진 시작 직전에는 두 시간 값을 같게 맞춤
 		currentTime = time.QuadPart;
 		previousTime = currentTime; //전프레임 타임에 시간을 안넣으면 값이 커짐.
@@ -41,14 +41,14 @@ namespace Wanted
 		while (!isQuit)
 		{
 			//timeGetTime(); 밀리초 단위 검색 1/1000 = 0.001초 
-			
+
 			//현재 시간 구하기
-			QueryPerformanceCounter(&time);  
+			QueryPerformanceCounter(&time);
 			currentTime = time.QuadPart;
 
 
 			// 프레임 시간 계산
-			float deltaTime 
+			float deltaTime
 				= static_cast<float>(currentTime - previousTime);
 
 			//초단위 변환
@@ -65,11 +65,20 @@ namespace Wanted
 
 				//이전 시간 값 갱신
 				previousTime = currentTime;
+
+				// 현재 입력 값을 이전 입력 값으로 저장
+				for (int ix = 0; ix < 255; ix++)
+				{
+					keyStates[ix].wasKeyDown = keyStates[ix].isKeyDown;
+				}
+
+
 			}
 
 		}
 
 		// Todo : 정리 작업 
+		std::cout << "Engine has been shutdown...\n";
 
 
 	}
@@ -77,14 +86,42 @@ namespace Wanted
 	{
 		isQuit = true;
 	}
+	bool Engine::GetKeyDown(int keyCode)
+	{
+		return keyStates[keyCode].isKeyDown
+			&& !keyStates[keyCode].wasKeyDown;
+	}
+	bool Engine::GetKeyup(int keyCode)
+	{
+		return !keyStates[keyCode].isKeyDown
+			&& keyStates[keyCode].wasKeyDown;
+	}
+	bool Engine::GetKey(int keyCode)
+	{
+		return keyStates[keyCode].isKeyDown;
+	}
 	void Engine::ProcessInput()
 	{
+		// 키 마다의 입력 읽기 
+		// !!! 운영체제가 제공하는 기능을 사용할 수 밖에 없음
+		for (int ix = 0; ix < 255; ix++)
+		{
+			keyStates[ix].isKeyDown =
+				(GetAsyncKeyState(ix) & 0x8000) > 0 ? true : false;
+		}
+		// 호출될 때 키가 중단 또는 작동 되었는지 여부와 GetAsyncKeyState에 대한 이전 이후 키가 눌렸는지 확인
 	}
 	void Engine::Tick(float deltaTime)
 	{
 		std::cout
 			<< "DeltaTime: " << deltaTime
 			<< ", FPS: " << (1.0f / deltaTime) << "\n";
+
+		//ESC키 눌리면 종료
+		if (GetKeyDown(VK_ESCAPE))
+		{
+			QuitEngine();
+		}
 	}
 	void Engine::Draw()
 	{
--- Engine/Engine/Engine.h
+++ Engine/Engine/Engine.h
@@ -4,17 +4,35 @@ namespace Wanted
 {
 
 	//Main game engine class
-	class Engine
+	class Engine // 동작 행동
 	{
+		// 데이터 함수도 데이터로 쓸거임.
+		struct KeyState
+		{
+			// 현재 키가 눌렸는지
+			bool isKeyDown = false;
+
+			//이전 키가 눌렸는지
+			bool wasKeyDown = false;
+		};
+
 	public:
-	Engine();
-	~Engine();
+		Engine();
+		~Engine();
 
-	//엔진 루프(게임루프)
-	void Run();
+		//엔진 루프(게임루프)
+		void Run();
 
-	//엔진 종료 함수
-	void QuitEngine();
+		//엔진 종료 함수
+		void QuitEngine();
+
+		//입력 확인 함수
+		//이전에 입력이 안됐는데, 현재 입력이 됐으면 1번 호출
+		bool GetKeyDown(int keyCode);
+		//이전에 입력이 됐는데, 현재 입력이 취소됐으면   1번 호출
+		bool GetKeyup(int keyCode);
+		// 현재 놀렸으면 반복 호출
+		bool GetKey(int keyCode);
 
 	private:
 		//입력 처리 함수
@@ -27,10 +45,15 @@ namespace Wanted
 		void Draw();
 
 	private:
-	bool isQuit = false;
+		//엔진 종료 플래그
+		bool isQuit = false;
+
+		//키 상태 저장용 배열
+		KeyState keyStates[255] = {};
+
 	};
 
-	
-
 }
+
+
 
